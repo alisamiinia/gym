32987be84e64f01cca6ba31e8489e1da2218b8d1
alisamiinia <asameeinia@gmail.com>
2022-11-19 17:21:46 +0330

    coach serializers.py added

--- coach/views.py
+++ coach/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from .models import Coach
+#from .serializers import GymSerializer, CourseSerializer,CourseReadSerializer
+#from .models import Gym, Course
+#from .serializers import GymSerializer, CourseSerializer,CourseReadSerializer
+
+blacklist =["123456789","111111111","222222222","333333333","444444444"]
