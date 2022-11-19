1f51b0ed4dc088aa1a24b2949ac674ad41b26aa5
alisamiinia <asameeinia@gmail.com>
2022-11-19 17:50:20 +0330

    get_update_delete_coach added to coach view

--- coach/views.py
+++ coach/views.py
@@ -12,7 +12,7 @@ from .serializers import CoachSerializer
 blacklist =["123456789","111111111","222222222","333333333","444444444"]
 
 @api_view(['GET', 'POST'])
-def gym_view(request):
+def coach_view(request):
     if request.method == "GET":
         person = Coach.objects.all()
         return Response(CoachSerializer(person, many=True).data,
@@ -32,3 +32,29 @@ def gym_view(request):
             return Response(ser.data, status=status.HTTP_201_CREATED)
         else:
             return Response(ser.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    ############# blacklist baayad ezafe she ##########
+    ############# information view?? ##########
+
+@api_view(['PUT', 'GET', 'DELETE'])
+def get_update_delete_coach(request, pk):
+    try:
+        coach = Coach.objects.get(pk=pk)
+    except:
+        return Response({"error": "Not Found!"}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        ser = CoachSerializer(coach)
+        return Response(ser.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'PUT':
+        ser = CoachSerializer(coach, data=request.data)
+        if ser.is_valid():
+            ser.save()
+            return Response(ser.data, status=status.HTTP_200_OK)
+        else:
+            return Response(ser.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        coach.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
