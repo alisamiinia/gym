3a93b50b97cabc8b6af4e0bc4da91e417154c7e9
alisamiinia <asameeinia@gmail.com>
2022-11-19 17:14:21 +0330

    coach models updated

--- coach/models.py
+++ coach/models.py
@@ -1,14 +1,16 @@
 from django.db import models
-
+from accounts import models################################
 class Coach(models.Model):
     fullName = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
     age = models.IntegerField()
     weight = models.IntegerField()
     gender = models.IntegerField()
-    #userID =
-    #post = models.CharField(max_length=40)
-    #picUrl = models.ImageField(blank = True, null = True)
+    #userID = models.ForeignKey(user, on_delete=models.CASCADE)
+    
+    
+    picUrl = models.ImageField(blank = True, null = True)
+    
     #details = models.ManyToManyField
     #achievements = models.ManyToManyField
     
