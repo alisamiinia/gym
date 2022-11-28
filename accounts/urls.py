6b3961ef82180557d67af35a369335988cd57073
alisamiinia <asameeinia@gmail.com>
2022-11-28 17:36:57 +0330

    signup url added to accounts/urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,14 @@
-from .views import RegisterAPI
+# from .views import RegisterAPI
+# from knox import views as knox_views
+# from .views import LoginAPI
 from django.urls import path
-from knox import views as knox_views
-from .views import LoginAPI
+from . import views
 
 urlpatterns = [
-    path('api/register/', RegisterAPI.as_view(), name='register'),
-     path('api/login/', LoginAPI.as_view(), name='login'),
-    path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
+    # path('api/register/', RegisterAPI.as_view(), name='register'),
+    #  path('api/login/', LoginAPI.as_view(), name='login'),
+    # path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
+    path('user/signup/',views.CreateUserView.as_view()),
 ]
 #http://localhost:8000/api/register/
 #http://localhost:8000/api/login/
