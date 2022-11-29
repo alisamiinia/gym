245853fc6c4e05b0e449fd22f7a9f0581e7c904c
alisamiinia <asameeinia@gmail.com>
2022-11-29 18:06:48 +0330

    Achievement added to admin

--- coach/admin.py
+++ coach/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from .models import Coach, Detail
+from .models import Coach, Detail, Achievement
 
 
 #yasin
 #admin.site.register(Gym)
 admin.site.register(Coach)
 admin.site.register(Detail)
+admin.site.register(Achievement)
 #admin.site.register(Card)
