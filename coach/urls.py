61ea8cdc669323d1c763b17be27ec791ea16598e
alisamiinia <asameeinia@gmail.com>
2022-12-01 13:37:23 +0330

    coach urls.py edited

--- coach/urls.py
+++ coach/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from coach import views
 
 urlpatterns = [
-    path('get-coach', views.coach_view),
+    path('get-all-coach', views.coach_view),
     path('post-coach', views.coach_post),
     path('get-update-delete-coach/<int:pk>', views.get_update_delete_coach),
     
