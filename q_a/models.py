d6cec396c547afd00ab82a0ebd1ecca0d692f0dd
alisamiinia <asameeinia@gmail.com>
2023-01-05 01:30:00 +0330

    QuestionScore and AnswerScore model in q_a model

--- q_a/models.py
+++ q_a/models.py
@@ -15,3 +15,14 @@ class Answer(models.Model):
   category = models.CharField(max_length=250)
   writerId = models.IntegerField()
   questionId = models.IntegerField()
+
+
+class QuestionScore(models.Model):
+    userId = models.IntegerField()
+    questionId = models.IntegerField()
+    score = models.IntegerField()
+
+class AnswerScore(models.Model):
+    userId = models.IntegerField()
+    answerId = models.IntegerField()
+    score = models.IntegerField()
