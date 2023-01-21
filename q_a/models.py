aae3f758a103b595eeb0e0a4a5125bc36a5c6308
alisamiinia <asameeinia@gmail.com>
2023-01-22 00:42:08 +0330

    feat: q_a model updated

--- q_a/models.py
+++ q_a/models.py
@@ -4,7 +4,7 @@ class Question(models.Model):
   title = models.CharField(max_length=250)
   content = models.CharField(max_length=5000)
   # date = models.DateTimeField()
-  date = models.CharField(max_length=30)
+  date = models.CharField(max_length=30, null=True, blank=True)
   category = models.CharField(max_length=250)
   writerId = models.IntegerField()
   acceptedAnswerId = models.IntegerField(null=True, blank=True)
@@ -13,8 +13,7 @@ class Question(models.Model):
 class Answer(models.Model):
   content = models.CharField(max_length=5000)
   # date = models.DateTimeField()
-  date = models.CharField(max_length=30)
-  category = models.CharField(max_length=250)
+  date = models.CharField(max_length=30, null=True, blank=True)
   writerId = models.IntegerField()
   questionId = models.IntegerField()
 
